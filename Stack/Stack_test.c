e8f98c4ffd8437f364c4d015e265b0ef8fa65c76
Florent Delannoy <florent.delannoy@gmail.com>
2013-01-31 16:40:52 +1300

    Shouldn't be commented

--- Stack/Stack_test.c
+++ Stack/Stack_test.c
@@ -7,35 +7,35 @@ int main(int argc, char** argv){
 	int a=1, b=2, c=3, d=4, e=5, f=6;
 	int* result;
 
-	//printf("Pushing all data...\n");
+	printf("Pushing all data...\n");
 	push(l, &a);
 	push(l, &b);
 	push(l, &c);
 	push(l, &d);
 	push(l, &e);
 	push(l, &f);
-	//display(l);
+	display(l);
 
 	result = (int*)pop(l);
-	//printf("\nPopping an element... Got %d\n", *result);
-	//display(l);
+	printf("\nPopping an element... Got %d\n", *result);
+	display(l);
 
 	result = (int*)pop(l);
-	//printf("\nPopping an element... Got %d\n", *result);
+	printf("\nPopping an element... Got %d\n", *result);
 
-	//printf("Popping 4 elements...\n");
+	printf("Popping 4 elements...\n");
 	result = (int*)pop(l);
 	result = (int*)pop(l);
 	result = (int*)pop(l);
 	result = (int*)pop(l);
-	//display(l);
+	display(l);
 
-	//printf("\nRe-pushing two elements...\n");
+	printf("\nRe-pushing two elements...\n");
 	push(l, &a);
 	push(l, &f);
-	//display(l);
+	display(l);
 
-	//printf("\nCleaning up...\n");
+	printf("\nCleaning up...\n");
 	cleanup(l);
 	return 0;
 }
